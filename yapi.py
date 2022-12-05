163f6af6c9015f224b6a1534f39dc7c44238ba00
k1ngr4m <365717424@qq.com>
2022-12-06 00:58:07 +0800

    20221206 update:add yapiApi

--- yapi.py
+++ yapi.py
@@ -1,3 +1,6 @@
+import json
+import os.path
+
 import requests as requests
 
 
@@ -49,6 +52,14 @@ class Yapi:
             'limit': 50
         }
         try:
+            file_name = r'data/interface_list_cat.json'
+            if not os.path.exists(file_name):
+                with open(file_name, 'a', encoding='utf-8') as file:
+                    file.close()
+            else:
+                with open(file_name, 'w', encoding='utf-8') as file:
+                    file.truncate(0)
+            interface_list_cat = []
             response = requests.get(url=url, params=body, headers=headers).json()
             # print(response)
             errcode = response['errcode']
@@ -62,6 +73,14 @@ class Yapi:
                     title = data_list[i]['title']
                     # 打印接口信息
                     print(f'interface_id:{interface_id}\ttitle:{title}')
+                    interface_data_dict = {
+                        'interface_id': interface_id,
+                        'title': title
+                    }
+                    interface_list_cat.append(interface_data_dict)
+                with open(file_name, 'w', encoding='utf-8') as w_f:
+                    result = json.dumps(interface_list_cat, ensure_ascii=False)
+                    w_f.write(result)
             # 返回数据错误
             else:
                 print(errcode)
@@ -69,37 +88,98 @@ class Yapi:
             print(e)
 
     # 获取接口数据（有详细接口数据定义文档）
-    def get_interface_detail(self, interface_id):
-        url = self.url + '/api/interface/get'
-        headers = {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-        body = {
-            'id': interface_id,  # 接口id
-            'token': self.token
-        }
-        try:
-            response = requests.get(url=url, params=body, headers=headers).json()
-            # print(response)
-            errcode = response['errcode']
-            # 成功获取data
-            if errcode == 0:
-                data = response['data']
-                method = data['method']
-                title = data['title']
-                path = data['path']
-                req_body_other = data['req_body_other']
-                res_body = data['res_body']
-                print(f'method:{method}\ttitle:{title}\tpath:{path}\r请求参数:\r{req_body_other}')
-                print(f'返回数据:\r{res_body}')
-            # 返回数据错误
-            else:
-                print(errcode)
-        except Exception as e:
-            print(e)
+    def get_interface_detail(self):
+        interface_list_cat_filename = r'data/interface_list_cat.json'
+        interface_detail_filename = r'data/interface_detail.json'
+        with open(interface_list_cat_filename, 'r', encoding='utf-8') as file:
+            interface_list_cat_list = json.load(file)
+        # print(dict_list)
+        if not os.path.exists(interface_detail_filename):
+            with open(interface_detail_filename, 'a', encoding='utf-8') as file:
+                file.close()
+        else:
+            with open(interface_detail_filename, 'w', encoding='utf-8') as file:
+                file.truncate(0)
+
+        interface_detail_list = []
+        for i in range(len(interface_list_cat_list)):
+            interface_id = interface_list_cat_list[i]['interface_id']
+            url = self.url + '/api/interface/get'
+            headers = {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+            body = {
+                'id': interface_id,  # 接口id
+                'token': self.token
+            }
+            try:
+                response = requests.get(url=url, params=body, headers=headers).json()
+                # print(response)
+                errcode = response['errcode']
+                # 成功获取data
+                if errcode == 0:
+                    data = response['data']
+                    method = data['method']
+                    title = data['title']
+                    path = data['path']
+                    req_body_other = data['req_body_other']
+                    # print(req_body_other)
+                    # print(type(req_body_other))
+                    # req_body_other = req_body_other.replace('\n ', '')
+                    # req_body_other = req_body_other.replace('\"', '')
+                    res_body = data['res_body']
+                    interface_detail_dict = {
+                        'title': title,
+                        'method': method,
+                        'path': path,
+                        'req_body': req_body_other,
+                        'res_body': res_body
+                    }
+                    interface_detail_list.append(interface_detail_dict)
+                    print(f'method:{method}\ttitle:{title}\tpath:{path}\r')
+                    print(f'请求参数:\r{req_body_other}')
+                    print(f'返回数据:\r{res_body}')
+                # 返回数据错误
+                else:
+                    print(errcode)
+            except Exception as e:
+                print(e)
+        with open(interface_detail_filename, 'a', encoding='utf-8') as w_f:
+            result = json.dumps(interface_detail_list, ensure_ascii=False)
+            w_f.write(result)
+
+    # 写入api_data.txt
+    def write_api_data(self):
+        interface_detail_filename = r'data/interface_detail.json'
+        api_data_filename = r'data/api_data.txt'
+        if not os.path.exists(interface_detail_filename):
+            with open(api_data_filename, 'a', encoding='utf-8') as file:
+                file.close()
+        else:
+            with open(api_data_filename, 'w', encoding='utf-8') as file:
+                file.truncate(0)
+
+        with open(interface_detail_filename, 'r', encoding='utf-8') as file:
+            interface_detail_dict = json.load(file)
+            for i in range(len(interface_detail_dict)):
+                title = interface_detail_dict[i]['title']
+                method = interface_detail_dict[i]['method']
+                path = interface_detail_dict[i]['path']
+                req_body = interface_detail_dict[i]['req_body']
+                api_data_dict = {
+                    'id': i+1,
+                    'name': title,
+                    'url': path,
+                    "frontDev": 1,
+                    "param":req_body
+                }
+                api_data_dict = (str(api_data_dict)+'\r').replace(r'\n', '').replace(' ', '')
+                with open(api_data_filename, 'a+', encoding='utf-8') as w_f:
+                    w_f.write(api_data_dict)
 
 if __name__ == '__main__':
     yapi = Yapi()
-    yapi.get_cat_menu()
-    yapi.get_interface_list_cat(1379)
-    yapi.get_interface_detail(11421)
+    # yapi.get_cat_menu()
+    # yapi.get_interface_list_cat(1379)
+    # yapi.get_interface_detail()
+    yapi.write_api_data()
